274bc63974a994b51b8514afa51b921b522b10da
Andy Smith <andysmith@Andys-MacBook-Pro.local>
2018-02-27 01:30:18 -0600

    files linked

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+var alphabet:Array = ("abcdefghijklmnopqrstuvwxyz").split("");
+trace("this is the letter b...", alphabet[1]);
+trace(alphabet);
+/* outputs:
+this is the letter b... b
+a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+*/
+
